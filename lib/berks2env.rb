0dc805494a0f1702faf0a39501c11f8b06ab520f
Pete Cheslock <petecheslock@gmail.com>
2014-04-29 00:21:49 -0400

    Remove original script and first pass convert to library

--- lib/berks2env.rb
+++ lib/berks2env.rb
@@ -1,5 +1,51 @@
 require 'berks2env/version'
 
+require 'berkshelf'
+require 'chef/environment'
+
 module Berks2Env
-  # Hello World!
+  # This class takes and dumps out environment json files
+  class Convert
+    attr_reader :branch, :berkslockfile
+
+    def initialize(branch, berkslockfile)
+      @branch = branch
+      @berkslockfile = berkslockfile
+    end
+
+    def run
+      environment = Chef::Environment.new
+      environment.name(sanitize)
+      berksfile = Berkshelf::Lockfile.from_file(@berkslockfile)
+      locks = berksfile.graph.locks.inject({}) do |hash, (name, dependency)|
+        hash[name] = "= #{dependency.locked_version.to_s}"
+        hash
+      end
+      environment.cookbook_versions(locks)
+
+      environment.override_attributes({ :server_env => { :version => { :real => @branch, :virt => @branch }}})
+      envfile = File.open("#{@branch}.json", 'w')
+      envfile.write(environment.to_json)
+      envfile.close
+      if @branch.match(/\d+\.\d+\.\d+\z/)
+        xver = @branch.split('.')
+        xver = "#{xver[0]}.#{xver[1]}.LATEST"
+        # Create the major.minor.X environment to go with the major.minor.patch environment
+        environment.name(xver.gsub('.', '_'))
+        environment.override_attributes({ :server_env => { :version => { :real => @branch, :virt => xver }}})
+        envfile = File.open("#{xver.gsub('.', '_')}.json", 'w')
+        envfile.write(environment.to_json)
+        envfile.close
+      end
+    end
+
+    def sanitize
+      if @branch =~ /\d+\.\d+\.\d+/
+        @branch.gsub('.','_')
+      else
+        @branch.gsub(/[\-;:,.\/\\']/,'_')
+      end
+    end
+
+  end
 end
