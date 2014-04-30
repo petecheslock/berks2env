cb0927aec2a1875474ee0830f524188fb9ee842e
Pete Cheslock <petecheslock@gmail.com>
2014-04-29 22:26:27 -0400

    Break out environment for latest into own method
    Include environment as a instance variable

--- lib/berks2env.rb
+++ lib/berks2env.rb
@@ -11,34 +11,39 @@ module Berks2Env
     def initialize(branch, berkslockfile)
       @branch = branch
       @berkslockfile = berkslockfile
+      @environment = Chef::Environment.new
     end
 
     def run
       clean_branch = self.sanitize_branch
-      environment = Chef::Environment.new
-      environment.name(clean_branch)
+      @environment.name(clean_branch)
       berksfile = Berkshelf::Lockfile.from_file(@berkslockfile)
       locks = berksfile.graph.locks.inject({}) do |hash, (name, dependency)|
         hash[name] = "= #{dependency.locked_version.to_s}"
         hash
       end
-      environment.cookbook_versions(locks)
+      @environment.cookbook_versions(locks)
 
-      environment.override_attributes({ :server_env => { :version => { :real => @branch, :virt => @branch }}})
+      @environment.override_attributes({ :server_env => { :version => { :real => @branch, :virt => @branch }}})
       envfile = File.open("#{clean_branch}.json", 'w')
-      envfile.write(environment.to_json)
+      envfile.write(@environment.to_json)
       envfile.close
+      self.create_latest
+    end
+
+    def create_latest
       if @branch.match(/\d+\.\d+\.\d+\z/)
         xver = @branch.split('.')
         xver = "#{xver[0]}.#{xver[1]}.LATEST"
         # Create the major.minor.X environment to go with the major.minor.patch environment
-        environment.name(xver.gsub('.', '_'))
-        environment.override_attributes({ :server_env => { :version => { :real => @branch, :virt => xver }}})
+        @environment.name(xver.gsub('.', '_'))
+        @environment.override_attributes({ :server_env => { :version => { :real => @branch, :virt => xver }}})
         envfile = File.open("#{xver.gsub('.', '_')}.json", 'w')
-        envfile.write(environment.to_json)
+        envfile.write(@environment.to_json)
         envfile.close
       end
     end
+
 
     def sanitize_branch
       if self.branch =~ /\d+\.\d+\.\d+/
