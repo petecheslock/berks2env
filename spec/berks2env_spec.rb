698033e319aa68deedb0b6958198f37f5b8884c1
Pete Cheslock <petecheslock@gmail.com>
2014-05-01 15:31:58 -0400

    Add a basic level of testing to the project

--- spec/berks2env_spec.rb
+++ spec/berks2env_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 
 describe Convert do
   before :each do
-    @convert = Convert.new("1.2.3", "Berksfile.stub")
+    @convert = Convert.new('1.2.3', 'Berksfile.stub')
   end
 
-  describe "#new" do
-    it "takes two parameters and returns a Convert object" do
+  describe '#new' do
+    it 'takes two parameters and returns a Convert object' do
         @convert.should be_an_instance_of Convert
     end
+  end
+
+  describe '#initialize' do
+    it 'returns the correct branch name' do
+      @convert.branch.should eql '1.2.3'
+    end
+
+    it 'returns the correct berkshelf lockfile' do
+      @convert.berkslockfile.should eql 'Berksfile.stub'
+    end
+
   end
 end
